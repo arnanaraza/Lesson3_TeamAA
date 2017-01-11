89c1eaada8988cfa63cd811639edd145eec3c550
Araza <arnan.araza@wur.nl>
2017-01-11 17:02:05 +0100

    Improved indention and structure. Finalized the code.

--- R/LeapYearFunction.R
+++ R/LeapYearFunction.R
@@ -3,20 +3,22 @@
 is.leap <- function (year) {
   #class control condition
   if (!is.numeric(year)) {
-    stop ("class of argument year must be numeric") 
+    stop ("class of argument year must be numeric")
+  }
     
   #test the conditions of a leap year: divisible by 4,  not divisible by 100 but is divisble by 400
-  } else if  ((year %% 4 == 0) && (year %% 100 != 0) | (year %% 400 == 0)) {
-      return (TRUE)  
+   else if  ((year %% 4 == 0) && (year %% 100 != 0) | (year %% 400 == 0)) {
+      return (TRUE)
+   }
     
   #condition to set the range of the valid years
-  } else if (year <= 1581) {
+   else if (year <= 1581) {
       print ('the year is out of range')
+   }
       
   #condition if the argument is not a leap year
-  } else {
-    return (FALSE)
-      
-        }
+     else {
+      return (FALSE)
+       }
 }
 
